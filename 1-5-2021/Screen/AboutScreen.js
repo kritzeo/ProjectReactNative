6aa0c8fbc5814bde457e059292e6afcfc8bc793e
kritzeo <95302686+kritzeo@users.noreply.github.com>
2022-01-05 11:29:59 +0700

    Add files via upload

---
+++ 1-5-2021/Screen/AboutScreen.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+const AboutScreen = ({route}) => {
+    const {email} = route.params;
+    return (
+        <View style={styles.container}>
+            <Text>เกี่ยวกับ?_?</Text>
+            <Text>Email: {email}</Text>
+        </View>
+    )
+}
+
+export default AboutScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+
+    }
+})
